40cfd5dc1ce0295531248ee019ba515c43379fdc
BrunoJPP78 <brunojpoker@gmail.com>
2024-11-15 10:30:28 -0300

    Criando a validação para Tarefa e Usuario

--- src/main/java/com/exercicio/atividade2/projeto_back_end_ii/model/Tarefa.java
+++ src/main/java/com/exercicio/atividade2/projeto_back_end_ii/model/Tarefa.java
@@ -7,8 +7,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
@@ -19,10 +23,19 @@ import java.time.LocalDate;
 @Document(collection = "tarefas")
 public class Tarefa {
 
+    @Id
     private String id;
+
+    @NotBlank(message = "O título não pode estar vazio.")
     private String titulo;
+
+    @NotBlank(message = "A descrição não pode estar vazia.")
     private String descricao;
+
+    @NotNull(message = "A data de criação é obrigatória.")
     private LocalDate dataCriacao;
+
+    @NotNull(message = "A prioridade deve ser informada.")
     Prioridade prioridade;
 
     @JsonIgnore
--- src/main/java/com/exercicio/atividade2/projeto_back_end_ii/model/Usuario.java
+++ src/main/java/com/exercicio/atividade2/projeto_back_end_ii/model/Usuario.java
@@ -2,12 +2,17 @@ package com.exercicio.atividade2.projeto_back_end_ii.model;
 
 import com.exercicio.atividade2.projeto_back_end_ii.dto.UsuarioDto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email;
 
 import java.util.List;
 
@@ -19,8 +24,16 @@ public class Usuario {
 
     @Id
     private String id;
+
+    @NotBlank(message = "O nome é obrigatório.")
     private String nome;
+
+    @NotBlank(message = "O email é obrigatório.")
+    @Email(message = "O email deve ser válido.")
     private String email;
+
+    @NotBlank(message = "O telefone é obrigatório.")
+    @Pattern(regexp = "\\d{10,11}", message = "O telefone deve conter entre 10 e 11 dígitos numéricos.")
     private String telefone;
 
     @JsonIgnore
